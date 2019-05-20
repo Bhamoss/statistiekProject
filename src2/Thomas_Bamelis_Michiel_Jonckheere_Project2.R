5a4f34c0f7cf5704726a648d2f9569e0e58e299b
MichielJ1998 <michiel.jonckheere@live.be>
2019-05-21 00:41:21 +0200

    data proberen geod te zetten, datums van last_review nog niet kunnen fixen

--- src2/Thomas_Bamelis_Michiel_Jonckheere_Project2.R
+++ src2/Thomas_Bamelis_Michiel_Jonckheere_Project2.R
@@ -30,3 +30,37 @@ airbnb = merge(airbnb, brussel, all = TRUE)
 #   naive test, enkel op aantal rijen gebaseerd
 nrow(airbnb) == (nrow(gent) + nrow(antwerpen) + nrow(brussel))
 
+attributes(airbnb)$names
+# price == 0 -> NA
+airbnb$price[airbnb$price == 0] = NA
+# minimum_nights > 365 -> NA
+airbnb$minimum_nights[airbnb$minimum_nights > 365] = NA
+
+# TODO: is dit correct???
+# availability_365 < 365 -> full = FALSE
+# availability_365 == 365 -> full = TRUE
+airbnb$full[airbnb$availability_365 < 365] = FALSE
+airbnb$full[airbnb$availability_365 == 365] = TRUE
+
+airbnb$last_review[airbnb$last_review == ""] = NA
+
+# TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# last_review aanpassen brussel
+datum = as.Date("2019-04-13");
+airbnb$date_compiled[airbnb$city == "Brussel"] = as.Date(datum)
+
+# last_review aanpassen Gent
+datum = as.Date("2019-04-16")
+airbnb$date_compiled[airbnb$city == "Gent"] = as.Date(datum)
+
+# last_review aanpassen Antwerpen
+datum = as.Date("2019-04-20");
+airbnb$date_compiled[airbnb$city == "Antwerpen"] = as.Date(datum)
+
+
+attach(airbnb)
+
+
+
+
+detach(airbnb)
