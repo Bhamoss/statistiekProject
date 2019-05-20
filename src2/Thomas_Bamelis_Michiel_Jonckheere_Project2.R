31421cbda266adc099d8a73641b8e3a793d08951
MichielJ1998 <michiel.jonckheere@live.be>
2019-05-20 23:44:37 +0200

    r file vergeten op te slaan lel

--- src2/Thomas_Bamelis_Michiel_Jonckheere_Project2.R
+++ src2/Thomas_Bamelis_Michiel_Jonckheere_Project2.R
@@ -11,3 +11,22 @@
 ###############   Load and format data   ##################
 ###########################################################
 
+# set wd
+wd = dirname(rstudioapi::getActiveDocumentContext()$path); wd
+setwd(wd)
+
+# eerst alle data apart inlezen van Gent, Antwerpen en Brussel
+gent = read.csv("listingsGent.csv", header = TRUE, sep = ",", dec = ".")
+gent$city = "Gent"
+antwerpen = read.csv("listingsAntwerpen.csv", header = TRUE, sep = ",", dec = ".")
+antwerpen$city = "Antwerpen"
+brussel = read.csv("listingsBrussel.csv", header = TRUE, sep = ",", dec = ".")
+brussel$city = "Brussel"
+
+# merge de 3 data frames tesamen 
+airbnb = merge(gent, antwerpen, all = TRUE)
+airbnb = merge(airbnb, brussel, all = TRUE)
+# test of dat alle rows van de drie data frames in de nieuwe dataframe zitten
+#   naive test, enkel op aantal rijen gebaseerd
+nrow(airbnb) == (nrow(gent) + nrow(antwerpen) + nrow(brussel))
+
