22b17fa7e16a7112da55c16c5dfc29fe837e530d
Bhamoss <26162072+Bhamoss@users.noreply.github.com>
2019-05-25 12:47:21 +0200

    fixes and 1 start

--- src2/Thomas_Bamelis_Michiel_Jonckheere_Project2.R
+++ src2/Thomas_Bamelis_Michiel_Jonckheere_Project2.R
@@ -23,29 +23,36 @@ antwerpen$city = "Antwerpen"
 brussel = read.csv("listingsBrussel.csv", header = TRUE, sep = ",", dec = ".")
 brussel$city = "Brussel"
 
-# merge de 3 data frames tesamen 
-airbnb = merge(gent, antwerpen, all = TRUE)
-airbnb = merge(airbnb, brussel, all = TRUE)
+# dataframes samenvoegen
+airbnb = rbind(gent, antwerpen, brussel)
 # test of dat alle rows van de drie data frames in de nieuwe dataframe zitten
 #   naive test, enkel op aantal rijen gebaseerd
 nrow(airbnb) == (nrow(gent) + nrow(antwerpen) + nrow(brussel))
+names(airbnb[airbnb$city == "Gent",]) ; names(gent)
+nrow(airbnb[airbnb$city == "Gent",]) ; nrow(gent)
+comp = airbnb[airbnb$city == "Gent",]
+
+head(comp); head(gent)
 
 attributes(airbnb)$names
+
+
 # price == 0 -> NA
 airbnb$price[airbnb$price == 0] = NA
 # minimum_nights > 365 -> NA
 airbnb$minimum_nights[airbnb$minimum_nights > 365] = NA
 
-# TODO: is dit correct???
-# availability_365 < 365 -> full = FALSE
-# availability_365 == 365 -> full = TRUE
-airbnb$full[airbnb$availability_365 < 365] = FALSE
-airbnb$full[airbnb$availability_365 == 365] = TRUE
+# Full == verhuurd == 0 dagen vrij
+airbnb$full = FALSE
+airbnb$full[airbnb$availability_365 == 0] = TRUE
 
 airbnb$last_review[airbnb$last_review == ""] = NA
+airbnb$last_review = as.Date(airbnb$last_review)
 
 # TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # last_review aanpassen brussel
+airbnb$date_compiled = as.Date("2019-04-13")
+
 datum = as.Date("2019-04-13");
 airbnb$date_compiled[airbnb$city == "Brussel"] = as.Date(datum)
 
@@ -57,10 +64,27 @@ airbnb$date_compiled[airbnb$city == "Gent"] = as.Date(datum)
 datum = as.Date("2019-04-20");
 airbnb$date_compiled[airbnb$city == "Antwerpen"] = as.Date(datum)
 
+airbnb$last_review =  as.integer(airbnb$date_compiled) - as.integer(airbnb$last_review)
+# airbnb$last_review[is.na(airbnb$last_review)] = 
 
 attach(airbnb)
 
 
+###########################################################
+###############   Ligging en type verblijf   ##############
+###########################################################
 
+### Type verblijf
+table(airbnb$room_type) # bevat geen NA's
 
-detach(airbnb)
+# hoofdstuk 14 & 15 zijn hier belangrijk voor
+# gemiddelde huurprijs, dus waarschijnlijk enkel H15
+
+# wat shit van H14 erop toegepast
+rt.lm = lm(price~room_type)
+summary(rt.lm)
+rt.an = aov(price~room_type)
+summary(rt.an)
+# iets aan rt is zeker signigicant
+
+boxplot(price[room_type])
